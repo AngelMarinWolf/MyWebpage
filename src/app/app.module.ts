0b460201d82e8b35d8280dc6303fa1d87939a873
Angel Marin <marin.miguelangel96@gmail.com>
2017-12-17 19:05:01 -0600

    Fixing Logo and headline backgroud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { HeadlineComponent } from './components/headline/headline.component';
+import { AboutComponent } from './components/about/about.component';
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 
 
 @NgModule({
@@ -13,10 +15,12 @@ import { HeadlineComponent } from './components/headline/headline.component';
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HeadlineComponent
+    HeadlineComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ScrollToModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
